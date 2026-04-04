47c67e00993f5118738aba3f2bae1de38209a614
FaizzE <faizulislam3540@gmail.com>
2026-04-04 22:50:27 +0530

    Added Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,6 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div data-theme="synthwave">
-      <button className="btn btn-primary">Click me</button>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-
       <Routes>
         <Route path="/" element={<HomePage />} />
 
